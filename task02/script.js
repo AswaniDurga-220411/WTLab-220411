fd6883398ac5c9ac235156a2ee75e13f961238d3
AswaniDurga-220411 <n220411@rguktn.ac.in>
2026-04-02 23:41:05 +0530

    separate the css and js file from html

---
+++ task02/script.js
@@ -0,0 +1,8 @@
+
+        let selectedItem="";
+        let selectedPrice=0;
+        function selectItem(itemName,price){
+            selectedItem=itemName;
+            selectedPrice=price;
+            alert("You selected:"+itemName);
+        }
